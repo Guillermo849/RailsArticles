1051cf31b1c582cede71db4d4dbf36577ecef4f1
Guillermo <orivealonsoguillermo@gmail.com>
2024-04-05 14:58:33 +0200

    Create users validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,8 @@ class UsersController < ApplicationController
   def create
     @user = Users.new(params.require(:users).permit(:first_name, :surname, :age))
     if @user.save
-      flash[:success] = "New to-do item successfully added!"
       redirect_to users_url
     else
-      flash.now[:error] = "To-do item creation failed"
       render :new
     end
   end
