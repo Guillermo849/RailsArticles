6b196d9172ca2cae03555e958375effdeb061a53
Guillermo <orivealonsoguillermo@gmail.com>
2024-04-10 15:06:14 +0200

    Create CURD for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,24 +1,59 @@
 # frozen_string_literal: true
 
 class ArticlesController < ApplicationController
-  def create
-    if @user = User.find(params[:user_id])
-      @article = Article.new(article_params)
-      if @article.save
-        flash[:success] = 'New users successfully created'
-        redirect_to users_url
-      else
-        flash.now[:danger] = 'User creation failed'
-        render :new
-      end
+  before_action :set_article, only: %i[show edit update destroy]
 
+  def index
+    @article = Article.all
+  end
+
+  def new
+    @article = Article.new
+    render :new
+  end
+
+  def edit; end
+
+  def show; end
+
+  def update
+    if @article.update(article_params)
+      flash[:success] = 'Article successfully updated'
+      redirect_to article_path
     else
-      flash.now[:danger] = 'User creation failed'
+      flash.now[:danger] = 'Article update failed'
+      render :edit
+    end
+  end
+
+  def destroy
+    if @article.destroy
+      flash[:success] = 'Article successfully deleted'
+      redirect_to articles_url
+    else
+      flash[:danger] = "Article couldn't be deleted"
+    end
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      flash[:success] = 'New article successfully created'
+      redirect_to article_url(@article.id)
+    else
+      flash.now[:danger] = 'Article creation failed'
       render :new
     end
   end
 
   private
+
+  def set_article
+    @article = Article.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:danger] = 'Article not found'
+    redirect_to articles_url
+  end
 
   def article_params
     params.require(:article).permit(:title, :body, :user_id)
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
   validates :title, :body, :user_id, presence: true
+  validates :user_id, numericality: { only_integer: true }
 end
